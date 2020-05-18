90d899ab6eebfc69fc6fdde5f574d73c4973773a
Rakesh Mothukuri <rockey5520@gmail.com>
2020-05-18 07:40:36 +0100

    right rotation by n times in an efficient manner.

--- Problems/Right Rotation/src/Main.java
+++ Problems/Right Rotation/src/Main.java
@@ -30,14 +30,4 @@ class Main {
         }
 
     }
-
-    public void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
-    }
 }
